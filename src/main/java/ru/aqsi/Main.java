0024cdd67f82385c8880a1af28ae8f99ea2a9b35
123alexher123-hash <123alexher123@gmail.com>
2025-11-20 11:35:31 +0300

    Update Main.java

--- src/main/java/ru/aqsi/Main.java
+++ src/main/java/ru/aqsi/Main.java
@@ -3,7 +3,9 @@ package ru.aqsi;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+    private static String msg = ""Hello aqsi.ru!!!!!!""
     public static void main(String[] args) {
-        System.out.printf("Hello aqsi.ru!!!!!!");
+        System.out.printf(msg);
     }
 }
