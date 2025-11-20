d313b7b424d423d792dd9b134e87b78e048e0e4d
123alexher123-hash <123alexher123@gmail.com>
2025-11-20 11:45:26 +0300

    Update Main.java

--- src/main/java/ru/aqsi/Main.java
+++ src/main/java/ru/aqsi/Main.java
@@ -4,7 +4,7 @@ package ru.aqsi;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
-    private static String msg = ""Hello aqsi.ru!!!!!!""
+    private static String msg = ""Hello aqsi.ru!!!""
     public static void main(String[] args) {
         System.out.printf(msg);
     }
