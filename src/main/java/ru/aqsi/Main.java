e6c3a3947bf1caa370315f26e09802d41896de46
Alexandr Yakovlev <123alexher123@gmail.com>
2025-11-20 11:36:42 +0300

    added getMessage() message

--- src/main/java/ru/aqsi/Main.java
+++ src/main/java/ru/aqsi/Main.java
@@ -4,6 +4,10 @@ package ru.aqsi;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello aqsi.ru!");
+        System.out.println("Hello aqsi.ru!");
+    }
+
+    private static String getMessage () {
+        return "Hello ru.aqsi";
     }
 }
